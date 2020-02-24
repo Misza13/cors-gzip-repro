ad05474ae25ff468af15379f8a1d263d0dd90015
Misza <misza@misza.net>
2020-02-24 12:15:35 +0100

    repro of Chalice bug with CORS + gzip

---
+++ app.py
@@ -0,0 +1,23 @@
+import gzip
+import json
+
+from chalice import Chalice, Response
+
+app = Chalice(app_name='cors-gzip-repro')
+app.api.binary_types.append('application/json')  # This breaks OPTIONS
+
+
+@app.route('/', methods=['GET'], cors=True)
+def index():
+    data = {'hello': 'world'}
+    serialized = json.dumps(data)
+    blob = gzip.compress(serialized.encode('utf-8'))
+
+    return Response(
+        body=blob,
+        status_code=200,
+        headers={
+            'Content-Type': 'application/json',
+            'Content-Encoding': 'gzip'
+        }
+    )
